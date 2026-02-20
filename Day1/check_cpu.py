4766981b0b47e709f6a37b3e92b288d9b827733e
pankajnaik211 <pankajnaik173@gmail.com>
2026-02-21 00:07:24 +0530

    correct the spellings

--- Day1/check_cpu.py
+++ Day1/check_cpu.py
@@ -1,11 +1,11 @@
 import psutil  # import the labrary for get cpu utilization from the script 
 
 def cpu_utilization():
-    thrash_hold= int(input("Enter your thrash hold"))# take user input of the trshhold to compare with cpu usages
+    thre_shold= int(input("Enter your thrash hold"))# take user input of the trshhold to compare with cpu usages
     current_cpu= psutil.cpu_percent(interval=1)   # to get Current cpu utilzation syntax of get psutil lbrary
 
-    if current_cpu > thrash_hold:  #use conditional statement for compare the condition  
-        print("Cpu alet mail has been send to user")
+    if current_cpu > thre_shold:  #use conditional statement for compare the condition  
+        print("Cpu alert mail has been send to user")
     else :
         print("Cpu is in safe state")   
 
